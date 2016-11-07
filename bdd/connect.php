df52eadc89f35d9cd921abf37efd9e5993cd19f3
Rovi44 <rod.villa94@gmail.com>
2016-11-06 21:06:59 -0600

    cambios header

--- bdd/connect.php
+++ bdd/connect.php
@@ -1,6 +1,6 @@
 <?php
-	$host = 'localhost';
-	$con = new mysqli($host,'root','','vivero');
+$host = 'localhost';
+	$con = new mysqli($host,'proyectoVivero','bX82e7cpfbYdj3e5','vivero');
 	if($con -> connect_errno)
 	{
 		echo "Fallo al conectar a mysql: (" . $con->connect_errno.")".$con->connect_error;
